6b9eb7791950d9467a405583458ee7dc306c038e
Njeru Cyrus <njerucyrus5@gmail.com>
2020-05-26 12:40:01 +0300

    first commit

---
+++ app/src/main/java/com/apps/mydashboard2020/MainActivity.java
@@ -0,0 +1,17 @@
+package com.apps.mydashboard2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    String you="Programming in Android";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(you,"The Oncreate()Event");
+    }
+}
